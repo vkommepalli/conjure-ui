491ca6c87d589255c709a92dc8ba86ce5f8096e7
venkat <kvenkatreddi@gmail.com>
2016-10-22 14:12:02 -0500

    updates

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,27 @@
 angular.module('conjure-ui').config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
-      .when('/', {
+      .when('/facilities', {
         templateUrl: 'views/shelterlist.html',
         controller: 'shelterController',
         controllerAs: 'shelterController'
-      });
+      })
+        .when('/clients', {
+            templateUrl: 'views/shelterlist.html',
+            controller: 'shelterController',
+            controllerAs: 'shelterController'
+        })
+        .when('/reservations', {
+            templateUrl: 'views/shelterlist.html',
+            controller: 'shelterController',
+            controllerAs: 'shelterController'
+        })
+        .when('/', {
+            templateUrl: 'views/about.html',
+            controller: 'shelterController',
+            controllerAs: 'shelterController'
+        })
+    ;
 
     $locationProvider.html5Mode(false);
   }]);
